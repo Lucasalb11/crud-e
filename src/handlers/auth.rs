1b8481d7a29b2a01805ba789ee724a2388288ee8
Lucas de Almeida <lucasalb11@gmail.com>
2025-07-25 15:35:11 -0300

    adicao de autenticacao com login

---
+++ src/handlers/auth.rs
@@ -0,0 +1,40 @@
+#[derive(Debug, Clone)]
+pub struct user {
+    pub username: String,
+    pub password: String,
+}
+
+pub fn get_users() -> Vec<User> {
+    vec![
+        User {
+            username: "exodus".into(),
+            password: "exodus123".into(),
+        }, 
+        
+
+    ]
+}
+
+use tide::{Request, Response, StatusCode};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct LoginPayload {
+    pub username: String,
+    pub password: String,
+}
+
+pub async fn login(mut req: Request<()>) -> tide::Result {
+    let payload: LoginPayload = req.body_json().await?;
+    let users = get_users();
+
+    let user_valid = users.iter().any(|u| {
+        u.username == payload.username && u.password == payload.password
+    });
+
+    if user_valid {
+        Ok(Response::new(StatusCode::Ok))
+    } else {
+        Ok(Response::new(StatusCode::Unauthorized))
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ use handlers::create::create_data;
 use handlers::delete::delete_data;
 use handlers::read::{read_all_data, read_data};
 use handlers::update::update_data;
+use tide::Request;
+use handlers::auth::login;
+
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
@@ -21,6 +24,7 @@ async fn main() -> tide::Result<()> {
     app.at("/data/:id").get(read_data); // Lê um
     app.at("/data/:id").put(update_data); // Atualiza
     app.at("/data/:id").delete(delete_data); // Deleta
+    app.at("/login").post(login); // Login
 
     let addr = "127.0.0.1:8080";
     println!("Servidor CRUD rodando em: http://{addr}");
